ceea57f04b98596a24754af3503b688df6021f35
Adrián González Rus <adrian.gonzalez.rus@bbva.com>
2016-02-12 08:26:40 +0100

    Rules documentation

--- htmlbars-minifier.js
+++ htmlbars-minifier.js
@@ -15,15 +15,43 @@ HtmlbarsMinifier.prototype.constructor = HtmlbarsMinifier;
 HtmlbarsMinifier.prototype.extensions = ['hbs'];
 HtmlbarsMinifier.prototype.targetExtension = 'hbs';
 HtmlbarsMinifier.prototype.processString = function (str) {
-    str = str.replace(/{{(#|\/|else)/g, '{{~$1');
-    str = str.replace(/\s{2,}/g, ' ');
-    str = str.replace(/>\s*([^\s])/g, '>$1');
-    str = str.replace(/([^\s])\s*</g, '$1<');
-    str = str.replace(/}}\s*</g, '}}<');
-    str = str.replace(/>\s*{{/g, '>{{');
-    str = str.replace(/^\s*/, '');
-    str = str.replace(/\s*$/, '');
-    str = str.replace(/(<.+?>[^<>]*?}})(\s*)({{[^<>]*?<.+?>)/g, '$1$3');
+    str = str
+        // Replaces:
+        //   - {{#foo}} => {{~#foo}}
+        //   - {{/foo}} => {{~/foo}}
+        .replace(/{{(#|\/|else)/g, '{{~$1')
+
+        // Replaces:
+        //   - "       " => " "
+        .replace(/\s{2,}/g, ' ')
+
+        // Replaces:
+        //   - ">           foo" => ">foo"
+        .replace(/>\s+/g, '>')
+
+        // Replaces:
+        //   - "foo           <" => "foo<"
+        .replace(/\s+</g, '<')
+
+        // Replaces:
+        //   - "}}      <" => "}}<"
+        .replace(/}}\s+</g, '}}<')
+
+        // Replaces:
+        //   - ">      {{" => ">{{"
+        .replace(/>\s+{{/g, '>{{')
+
+        // Replaces:
+        //   - "     foo" => "foo"
+        .replace(/^\s+/, '')
+
+        // Replaces:
+        //   - "foo     " => "foo"
+        .replace(/\s+$/, '')
+
+        // Replaces:
+        //   - "<foo>{{bar}}            {{bar}}</foo>" => "<foo>{{bar}}{{bar}}</foo>"
+        .replace(/(<.+?>[^<>]*?}})(\s+)({{[^<>]*?<.+?>)/g, '$1$3');
 
     return str;
 };
