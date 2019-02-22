7ecf34d2d2832b225ac7e5b3cd9c21846797e562
Juan Antonio Gómez Benito <juanantonio.gomez.benito@bbva.com>
2019-02-22 09:15:18 +0100

    ci(husky): removed alone npm run

--- .huskyrc.js
+++ .huskyrc.js
@@ -3,6 +3,6 @@
 module.exports = {
 	hooks: {
 		'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-		'pre-push': 'npm run lint:js && npm run && npm test'
+		'pre-push': 'npm run lint:js && npm test'
 	}
 };
